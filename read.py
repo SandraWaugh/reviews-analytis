c7d0ab1144bf1a8d52d215b0309601ff3edd387d
Sandra <kshysei@gmail.com>
2021-05-12 18:44:11 +0900

    cal len

--- read.py
+++ read.py
@@ -6,6 +6,11 @@ with open ("reviews.txt","r") as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
 
-	
+print("檔案讀取完了，總共有",len(data),"個資料")
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+print("コメントの平均長さは", sum_len/len(data))
